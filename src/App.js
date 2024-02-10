ce86534fb746ab20560a1aae83419975c6cadea3
MilosJovic23 <milos.mj992@gmail.com>
2024-02-10 13:26:57 +0100

    * initial commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
-import  axios  from 'axios';
 
- console.log(process.env.REACT_APP_OMDBAPI_KEY)
+import SearchBar from "./Components/SearchBar";
 
-    console.log(process.env.REACT_APP_URL+"?t=Terminator&apikey="+process.env.REACT_APP_OMDBAPI_KEY)
-    axios.get(process.env.REACT_APP_URL+"?t=Terminator&apikey="+process.env.REACT_APP_OMDBAPI_KEY)
-    .then(response=>console.log(response))
-        .catch(error=>console.log(error))
+ // console.log(process.env.REACT_APP_OMDBAPI_KEY)
+ //
+ //    console.log(process.env.REACT_APP_URL+"?t=Terminator&apikey="+process.env.REACT_APP_OMDBAPI_KEY)
+ //    axios.get(process.env.REACT_APP_URL+"?t=Terminator&apikey="+process.env.REACT_APP_OMDBAPI_KEY)
+ //    .then(response=>console.log(response))
+ //        .catch(error=>console.log(error))
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ImdbSearch = ()=> {
+    return (
+        <>
+            <h1>Test</h1>
+            <SearchBar/>
+        </>
+
+    )
 }
-
-export default App;
+export default ImdbSearch
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,30 @@
+
+
+import  axios  from 'axios';
+import {useState} from "react";
+
+const SearchBar= ()=>{
+    const [searchTitle,setSearchTitle]=useState()
+    const searchMovies=()=>{
+
+
+        // console.log(searchTitle)
+        axios.get(process.env.REACT_APP_URL+"?t="+searchTitle+"&apikey="+process.env.REACT_APP_OMDBAPI_KEY)
+            .then(response=>console.log(response))
+            .catch(error=>console.log(error))
+
+    }
+
+
+
+    return(
+      <form>
+          <input type="text" onInput={e=>{setSearchTitle(e.target.value)}}/>
+          <button type="button" onClick={searchMovies}>Search Movies</button>
+      </form>
+    )
+
+}
+
+
+export default SearchBar
