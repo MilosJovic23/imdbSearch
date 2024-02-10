6b3dfef9faf9beafb382e13e3d97caa5dfac5e8e
MilosJovic23 <milos.mj992@gmail.com>
2024-02-10 13:28:13 +0100

    * search bar added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import './App.css';
 
 import SearchBar from "./Components/SearchBar";
 
- // console.log(process.env.REACT_APP_OMDBAPI_KEY)
- //
- //    console.log(process.env.REACT_APP_URL+"?t=Terminator&apikey="+process.env.REACT_APP_OMDBAPI_KEY)
- //    axios.get(process.env.REACT_APP_URL+"?t=Terminator&apikey="+process.env.REACT_APP_OMDBAPI_KEY)
- //    .then(response=>console.log(response))
- //        .catch(error=>console.log(error))
-
-
 const ImdbSearch = ()=> {
     return (
         <>
